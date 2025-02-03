1da41ae5555569bdc9f6dd1f2a144d71827a30f2
Maynard <emaynardvp@gmail.com>
2025-02-02 21:51:32 -0600

    Debugging

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Navbar from "../components/Navbar";
+import Navbar from "@/components/Navbar";
 
 import { AuthProvider } from "@/context/AuthContext";
 import { CartProvider } from "@/context/CartContext";
