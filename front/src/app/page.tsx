eb47053d9b0fccf610ec2559dcb37e1bee620f01
Maynard <emaynardvp@gmail.com>
2025-03-06 09:39:53 -0600

    Starting changes

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -6,7 +6,7 @@ const Home: React.FC = () => {
         
       {/* Header */}
       <div className="text-center mb-8">
-        <h1 className="text-4xl font-bold text-gray-800">Welcome to Our Store</h1>
+        <h1 className="text-4xl font-bold text-gray-800">Welcome to the Thecnologiest Store!</h1>
         <p className="text-lg text-gray-600 mt-2">Explore our range of products!</p>
       </div>
 
--- front/src/component/CartStatus.tsx
+++ front/src/component/CartStatus.tsx
@@ -19,7 +19,7 @@ export default function CartStatus() {
             height={48}
             className="w-12 h-12 rounded-full hover:opacity-90 transition-opacity"
           />
-          <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full">
+          <span className="absolute -top-2 -right-2 bg-red-800 text-white text-xs font-bold w-5 h-5 flex items-center justify-center rounded-full">
             {cartItems}
           </span>
         </div>
